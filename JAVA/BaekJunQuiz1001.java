1e466efb735a27edc5b3b9b093f183c374fdea78
TaeHyoung Kim <xdkvlago@gmail.com>
2019-02-05 20:08:37 +0900

    5 February add

---
+++ JAVA/BaekJunQuiz1001.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BaekJunQuiz1001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scanTH = new Scanner(System.in);
+
+int num1 = scanTH.nextInt();
+int num2 = scanTH.nextInt();
+
+int num3 = num1 - num2;
+
+System.out.println(num3);
+	}
+
+}
