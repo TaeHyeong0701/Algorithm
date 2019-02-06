3833954b5bc6e11c70138079048a9e7fbc2719ce
TaeHyoung Kim <xdkvlago@gmail.com>
2019-02-06 11:43:05 +0900

    6 February add

---
+++ JAVA/BaekJunQuiz1008.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BaekJunQuiz1008 {
+// 입력 예시 3 2
+// 출력 예시 1
+
+	    public static void main(String[] args){
+	        
+	         Scanner scan = new Scanner(System.in);
+	         double num1 = scan.nextInt();
+	         double num2 = scan.nextInt();
+	         
+	         System.out.println(num1/num2);
+	        
+	    }
+	}
